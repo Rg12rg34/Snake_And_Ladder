326a5e5a87bccb0a96bc4d09f74fdab3486f6a78
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-08 21:02:12 +0530

    Snake And Ladder Game At Start Position 0

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int playerposition = 0;
+        System.out.println("The position of player is : " + playerposition);// write your code here
+    }
+}
