45dc7e3db06fae607c0f43671c5d1f674ece8801
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-08 22:59:00 +0530

    Exact winning position

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,7 +15,7 @@ public class Main {
         System.out.println("The start position of player is : " + playerposition);
         System.out.println();
         Random ranval = new Random();
-        while (playerposition < 100) {
+        while (playerposition != 100) {
             int diceroll = ranval.nextInt(6) + 1;
             int check_play = ranval.nextInt(3);
             switch (check_play) {
@@ -24,7 +24,7 @@ public class Main {
                 case Ladder:
                     playerposition += diceroll;
                     if (playerposition > 100) {
-                        playerposition = 100;
+                        playerposition -= diceroll;
                     }
                     break;
                 case Snake:
